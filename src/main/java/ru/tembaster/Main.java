df0f449c3c0404a37a6f5eed13a4c2d5ea304c1e
Artem Astrakhantsev <tembaster@gmail.com>
2020-12-11 18:25:55 +0300

    model making 01

--- src/main/java/ru/tembaster/Main.java
+++ src/main/java/ru/tembaster/Main.java
@@ -4,15 +4,22 @@ import ru.tembaster.model.Dish;
 import ru.tembaster.model.Restaurant;
 import ru.tembaster.util.ConsoleHelper;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
         Restaurant restaurant = new Restaurant();
-        restaurant.setName("Чайхона №1");
-        restaurant.setMenu(List.of(new Dish("Плов", 400), new Dish("Цезарь", 350)));
-        ConsoleHelper.write(restaurant.toString() + ": " + restaurant.getMenu());
+        restaurant.setName("Чайхона");
+        Dish dish01 = new Dish("Плов", 400);
+        Dish dish02 = new Dish("Цезарь", 350);
+        Dish dish03 = new Dish("Шашлык из свинины", 500);
+        Dish dish04 = new Dish("Шашлык из баранины", 450);
+        Dish dish05 = new Dish("Шашлык из курицы", 250);
+        Dish dish06 = new Dish("Чай с бергамотом", 100);
+        restaurant.setMenu(List.of(dish01, dish02, dish03, dish04, dish05, dish06));
     }
 
 }
