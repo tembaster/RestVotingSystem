c05e23c762e34d0ae2e984fbd3387fda1ef60802
Artem Astrakhantsev <tembaster@gmail.com>
2020-12-12 15:34:34 +0300

    model making 02

--- src/main/java/ru/tembaster/model/User.java
+++ src/main/java/ru/tembaster/model/User.java
@@ -19,5 +19,46 @@ public class User extends AbstractBaseEntity {
         this.isVoted = isVoted;
     }
 
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Boolean getVoted() {
+        return isVoted;
+    }
+
+    public void setVoted(Boolean voted) {
+        isVoted = voted;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                ", isVoted=" + isVoted +
+                '}';
+    }
 }
